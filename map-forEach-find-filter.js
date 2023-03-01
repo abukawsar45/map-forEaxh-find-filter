45704cd298c56c0281021eee0eb335da8432b62d
abukawsar45 <kawsar1146236@gmail.com>
2023-03-01 14:55:53 +0600

    first commit

---
+++ map-forEach-find-filter.js
@@ -0,0 +1,17 @@
+const aks = [23, 63, 64, 34, 39, 74, 15];
+const x = aks.map(n => {
+    
+    return ('map number', n *3)
+});
+console.log(x)
+// 
+// aks.forEach(n => console.log(n));
+
+
+// const x = aks.find(n => n > 40);
+// console.log(x)
+// 
+
+// const x = aks.filter(n => n > 40);
+// console.log(x)
+// 
